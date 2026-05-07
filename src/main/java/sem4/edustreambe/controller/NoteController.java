ff9e259a713c1a7b8c8687973d8104f8d8fe0195
hailongtn <hailong0309.tn@gmail.com>
2026-05-08 05:09:07 +0700

    fix : permission notes

--- src/main/java/sem4/edustreambe/controller/NoteController.java
+++ src/main/java/sem4/edustreambe/controller/NoteController.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import sem4.edustreambe.dto.common.ApiResponse;
 import sem4.edustreambe.dto.note.request.NoteRequest;
@@ -22,6 +23,7 @@ public class NoteController {
     NoteService noteService;
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('USER', 'STUDENT')")
     ApiResponse<NoteResponse> createNote(@RequestBody @Valid NoteRequest request) {
         return ApiResponse.<NoteResponse>builder()
                 .result(noteService.createNote(request))
@@ -29,6 +31,7 @@ public class NoteController {
     }
 
     @GetMapping("/course/{courseId}")
+    @PreAuthorize("hasAnyRole('USER', 'STUDENT')")
     ApiResponse<List<NoteResponse>> getMyNotesByCourse(@PathVariable String courseId) {
         return ApiResponse.<List<NoteResponse>>builder()
                 .result(noteService.getMyNotesByCourse(courseId))
@@ -36,6 +39,7 @@ public class NoteController {
     }
 
     @DeleteMapping("/{noteId}")
+    @PreAuthorize("hasAnyRole('USER', 'STUDENT')")
     ApiResponse<Void> deleteNote(@PathVariable String noteId) {
         noteService.deleteNote(noteId);
         return ApiResponse.<Void>builder().build();
